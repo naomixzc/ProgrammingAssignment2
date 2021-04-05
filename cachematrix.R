d7b9128756d968d3b801918705b22d4557b0e3eb
naomixzc <79954847+naomixzc@users.noreply.github.com>
2021-04-05 14:13:34 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,18 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Week 3 assignment 2021 04 05 Developed by Naomi Xiao
+## It includes partial programming including two functions, one assigns values to new matrix, the other inverse it
 
-## Write a short comment describing this function
+## This function make a matrix object and is inversible
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL # inverse matrix, set to null
+        set <- function(y) { # assign values to new matrix
+                m <<- NULL
+                x <<- y
+        } 
+        get <- function() x # get the value of created matrix x
+        setinverse <- function(inverse) m <<- inverse
+        getinverse <- function() m
+        list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
@@ -12,4 +20,12 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        if(!is.null(m)){
+                message("Getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- inverse(data, ...)
+        x$setmean(m)
+        m
 }
